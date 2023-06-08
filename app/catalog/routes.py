55a393e4e5b5d24824023592c539199be3dde074
HiramHernandez <hhg.mzt@gmail.com>
2023-06-08 14:12:20 -0600

    chore: rutas que corresponden al crud

--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
-from flask import render_template
-
+from flask import render_template, request, flash, redirect, url_for
+from flask_login import login_required
+from app.catalog.forms import EditBookForm, CreateBookForm
 main = Blueprint('main', __name__, template_folder="templates")
 
 @main.route('/')
@@ -8,6 +9,8 @@ def display_books():
     from app import db
     from app.catalog.models import Book
     books = Book.query.all()
+    for book in books:
+        print(book.title)
     return render_template('home.html', books=books)
 
 
@@ -19,3 +22,51 @@ def display_publisher(publisher_id):
     publisher_books = Book.query.filter_by(pub_id = publisher.id).all()
 
     return render_template('publisher.html', publisher=publisher, publisher_books=publisher_books)
+
+@main.route('/book/delete/<book_id>', methods=['GET', 'POST'])
+@login_required
+def delete_book(book_id):
+    from app import db
+    from app.catalog.models import Book
+    book = Book.query.get(book_id)
+    if request.method == 'POST':
+        db.session.delete(book)
+        db.session.commit()
+        flash('Book deleted successfully')
+        return redirect(url_for('main.display_books'))
+    return render_template('delete_book.html', book=book, book_id=book.id)
+
+@main.route('/edit/book/<book_id>', methods=['GET', 'POST'])
+@login_required
+def edit_book(book_id):
+    from app import db
+    from app.catalog.models import Book
+    book = Book.query.get(book_id)
+    form = EditBookForm(obj=book)
+    if form.validate_on_submit():
+        book.title = form.title.data
+        book.format = form.format.data
+        book.num_pages = form.num_pages.data
+        db.session.add(book)
+        db.session.commit()
+        flash('Book Edited Successfully')
+        return redirect(url_for('main.display_books'))
+    return render_template('edit_book.html', form=form)
+
+
+@main.route('/create/book/<pub_id>', methods=['GET', 'POST'])
+@login_required
+def create_book(pub_id):
+    from app import db
+    from app.catalog.models import Book
+    form = CreateBookForm()
+    form.pub_id.data = pub_id  # pre-populates pub_id
+    if form.validate_on_submit():
+        book = Book(title=form.title.data, author=form.author.data, avg_rating=form.avg_rating.data,
+                    book_format=form.format.data, image=form.img_url.data, num_pages=form.num_pages.data,
+                    pub_id=form.pub_id.data)
+        db.session.add(book)
+        db.session.commit()
+        flash('Book added successfully')
+        return redirect(url_for('main.display_publisher', publisher_id=pub_id))
+    return render_template('create_book.html', form=form, pub_id=pub_id)
